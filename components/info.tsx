394bc6c1c048637b39bac1a0df0c8cc0c183b210
devcarol03 <devcarol03@gmail.com>
2024-01-18 21:02:37 +0000

    Description addition on Individual Product Page

--- components/info.tsx
+++ components/info.tsx
@@ -25,17 +25,24 @@ const Info: React.FC<InfoProps> = ({
             </div>
             <hr className="my-4"/>
             <div className="flex flex-col gap-y-6">
-                <div className="flex items-center gap-x-4" >
-                    <h3 className="font-semibold text-black">
-                        Size: 
-                    </h3>
-                    <div>
-                        {data?.size?.name}
+                <div className="flex items-center justify-around">
+                    <div className="flex items-center gap-x-4" >
+                        <h3 className="font-semibold text-black">
+                            Size: 
+                        </h3>
+                        <div>
+                            {data?.size?.name}
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-x-4">
+                        <h3 className="font-semibold text-black">Color:</h3>
+                        <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
                     </div>
                 </div>
                 <div className="flex items-center gap-x-4">
-                    <h3 className="font-semibold text-black">Color:</h3>
-                    <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
+                    <p className="pl-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
                 </div>
             </div>
             <div className="mt-10 flex items-center gap-x-3">
