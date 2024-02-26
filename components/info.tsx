44ca913ffc0373f5350e02e866a5dcb19e86d733
devcarol03 <devcarol03@gmail.com>
2024-02-26 19:22:32 +0000

    Add to Cart Update

--- components/info.tsx
+++ components/info.tsx
@@ -4,14 +4,19 @@ import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
     data: Product
-}
+};
 
-const Info: React.FC<InfoProps> = ({
-    data
-}) => {
+const Info: React.FC<InfoProps> = ({ data }) => {
+    const cart = useCart();
+  
+    const onAddToCart = () => {
+      cart.addItem(data);
+    }
+
     return ( 
         <div>
             <h1 className="text-3xl font-bold text-gray-900" >
@@ -47,7 +52,7 @@ const Info: React.FC<InfoProps> = ({
             </div>
             <hr className="my-4"/>
             <div className="mt-10 flex items-center gap-x-3">
-                <Button className="flex items-center rounded-full bg-black px-4 py-2">
+                <Button onClick={onAddToCart} className="flex items-center rounded-full bg-black px-4 py-2">
                     <ShoppingCart />
                     &nbsp;
                     Adicionar ao Carrinho
